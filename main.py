629bfd63aa997dd1c5f6234e11f72fa1a2a57e0f
EC2 Default User <ec2-user@ip-172-16-36-129.ec2.internal>
2023-04-10 11:54:09 +0000

    add enhance n

--- main.py
+++ main.py
@@ -40,38 +40,6 @@ def q_sizeStatistic(bigger):
     return sizeStatistic_result
 
 
-def q_sizeStatistic_7(bigger):
-    bigger_tmp_H = np.zeros(32, dtype=np.uint32)
-    bigger_tmp_V = np.zeros(25, dtype=np.uint32)
-    bigger_tmp_U = np.zeros(25, dtype=np.uint32)
-    bigger_tmp_D = np.zeros(25, dtype=np.uint32)
-
-    # bigger_tmp = bigger[0:26,6:26]
-    for i in range(25):
-        bigger_tmp_V[i] = bigger[i]
-        bigger_tmp_U[i] = bigger[i + 7] & 0x1ffffff
-        bigger_tmp_D[i] = bigger[i] & 0x1ffffff
-
-    for i in range(32):
-        bigger_tmp_H[i] = bigger[i] & 0x1ffffff
-
-    sizeStatistic_result = np.zeros(28, dtype=np.int32)
-
-    for j in range(1, 8):
-        for i in range(25):
-            bigger_tmp_V[i] &= bigger[i + j]
-            bigger_tmp_U[i] &= (bigger[i + 7 - j] >> j) & 0x1ffffff
-            bigger_tmp_D[i] &= (bigger[i + j] >> j) & 0x1ffffff
-
-        for i in range(32):
-            bigger_tmp_H[i] &= (bigger[i] >> j) & 0x1ffffff
-
-        sizeStatistic_result[j -1 + 0] = getSum(bigger_tmp_H)
-        sizeStatistic_result[j -1 + 7] = getSum(bigger_tmp_V)
-        sizeStatistic_result[j -1 + 14] = getSum(bigger_tmp_U)
-        sizeStatistic_result[j -1 + 21] = getSum(bigger_tmp_D)
-    return sizeStatistic_result
-
 
 #320 x 240, shift =2; 160 x 120, shift =1, 
 def subSmaple32x32(arr, INPUT_ROWS, INPUT_COLS, shift):
@@ -93,40 +61,6 @@ def subSmaple32x32(arr, INPUT_ROWS, INPUT_COLS, shift):
     
     return retarray
 
-
-def r_subSmaple32x32(arr, INPUT_ROWS, INPUT_COLS, shift):
-    HISTOGRAM_ROWS = int(32)
-    HISTOGRAM_COLS = int(32)
-
-    start_row = (INPUT_ROWS>>1) - (HISTOGRAM_ROWS<<(shift-1))
-    start_col = (INPUT_COLS>>1)
-    end_row = start_row + (HISTOGRAM_ROWS<<shift)
-    end_col = start_col + (HISTOGRAM_COLS<<shift)
-    step= (1<<shift)
-    retarray=arr[start_row:end_row:step, start_col:end_col:step]
-    
-    #for i in range(32):
-    #    print(" %3d",retarray[i])
-
-    
-    return retarray
-
-def l_subSmaple32x32(arr, INPUT_ROWS, INPUT_COLS, shift):
-    HISTOGRAM_ROWS = int(32)
-    HISTOGRAM_COLS = int(32)
-
-    start_row = (INPUT_ROWS>>1) - (HISTOGRAM_ROWS<<(shift-1))
-    start_col = (INPUT_COLS>>1) - (HISTOGRAM_COLS<<(shift))
-    end_row = start_row + (HISTOGRAM_ROWS<<shift)
-    end_col = start_col + (HISTOGRAM_COLS<<shift)
-    step= (1<<shift)
-    retarray=arr[start_row:end_row:step, start_col:end_col:step]
-    
-    #for i in range(32):
-    #    print(" %3d",retarray[i])
-
-    
-    return retarray
 
 
 
@@ -209,32 +143,7 @@ def calculate_28_percent(arr,arrSize):
 
     return 0
 
-import math
-def calculate_28_percent_narrow(arr,arrSize, boundSize):
-    freq=np.zeros(256)
-    sum_=0
-    for i in range(arrSize):
-        for j in range(arrSize):
-            freq[arr[i][j]] += 1
-            
-    ##if isPlot:
-    ##    one_d_array = np.ravel(arr)
-    ##    sorted_array = np.sort(one_d_array)
-    ##    plt.plot(sorted_array)
 
-
-    # 排序并找到 28% 阈值
-    #math.floor(x)
-    threshold=math.ceil(((arrSize-2*boundSize)**2)*0.28)-0.5
-
-
-    for i in range(255, -1, -1):
-        sum_ += freq[i]
-        #print(i, freq[i], sum_)
-        if sum_ > threshold:
-            return i
-
-    return 0
 
 import math
 def create_matrices(ref_image, theta , scale): # scale for 320x240 should be 4, for 160x120 should be 2
@@ -258,9 +167,9 @@ def create_matrices(ref_image, theta , scale): # scale for 320x240 should be 4, 
 def allFeature(arr):
         
     arr_32x32=subSmaple32x32(arr, INPUT_ROWS=240, INPUT_COLS=320, shift=2)
-    sizeStatistic_result=allFeature_core(arr_32x32)
+    t_value, sizeStatistic_result=allFeature_core(arr_32x32)
     # 计算 28% 的值并输出
-    return sizeStatistic_result
+    return t_value, sizeStatistic_result
 
 def allFeature_core(arr_32x32):
         
@@ -271,12 +180,12 @@ def allFeature_core(arr_32x32):
     bits_array = calculate_32x32_bits_array(arr_32x32, t_value)
     #show_32x32(arr_32x32,bits_array)
     sizeStatistic_result= q_sizeStatistic(bits_array)
-    return sizeStatistic_result
+    return t_value, sizeStatistic_result
 
 
 
 def allFeatureNorm(arr):
-    sizeStatistic_result=allFeature(arr);
+    t_value, sizeStatistic_result=allFeature(arr);
     sizeStatistic_result_norm = np.zeros(24, float)
     for i in range(4):
         _sum=1
@@ -285,10 +194,10 @@ def allFeatureNorm(arr):
         for j in range(6):
             sizeStatistic_result_norm[i*6+j]=sizeStatistic_result[i*6+j]/_sum
             
-    return sizeStatistic_result_norm
+    return t_value, sizeStatistic_result_norm
 
 def allFeatureNorm_fine(arr):
-    sizeStatistic_result=allFeature_core(arr);
+    t_value, sizeStatistic_result=allFeature_core(arr);
     sizeStatistic_result_norm = np.zeros(24, float)
     for i in range(4):
         _sum=1
@@ -297,7 +206,7 @@ def allFeatureNorm_fine(arr):
         for j in range(6):
             sizeStatistic_result_norm[i*6+j]=sizeStatistic_result[i*6+j]/_sum
             
-    return sizeStatistic_result_norm
+    return t_value, sizeStatistic_result_norm
 
 
 
